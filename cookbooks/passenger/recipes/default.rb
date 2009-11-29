7cd8ff08d383ffc522ba95201cf5d1ec606e74a1
Matthew Landauer <matthew@openaustralia.org>
2009-11-29 18:54:56 +1100

    Now loads up the modules for passenger into apache

--- cookbooks/passenger/recipes/default.rb
+++ cookbooks/passenger/recipes/default.rb
@@ -1,3 +1,5 @@
+require_recipe 'apache'
+
 gem_package "passenger" do
   version node[:passenger][:version]
 end
@@ -6,3 +8,15 @@ execute "passenger_module" do
   command 'passenger-install-apache2-module -a'
   creates node[:passenger][:module_path]
 end
+
+template "#{node[:apache][:dir]}/mods-available/passenger.load" do
+  source "passenger.load.erb"
+  mode 0755
+end
+
+template "#{node[:apache][:dir]}/mods-available/passenger.conf" do
+  source "passenger.conf.erb"
+  mode 0755
+end
+ 
+apache_module "passenger"
