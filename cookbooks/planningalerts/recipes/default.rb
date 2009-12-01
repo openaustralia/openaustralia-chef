be51aab34433bdd4c2fcff799fa980b1eb4acd46
Matthew Landauer <matthew@openaustralia.org>
2009-12-01 15:01:54 +1100

    Removing thin again because I got passenger working on the production server (finally). It turned out that ruby needed recompiling.

--- cookbooks/planningalerts/recipes/default.rb
+++ cookbooks/planningalerts/recipes/default.rb
@@ -6,7 +6,7 @@ require_recipe 'apache'
 require_recipe 'php'
 require_recipe 'mysql'
 #require_recipe 'passenger'
-require_recipe 'thin'
+#require_recipe 'thin'
 
 # For the time being only setting up the staging environment (:test)
 [:test].each do |stage|
