9ff6821ce31d5bba6b48d8a0ec12d352c4372b80
Matthew Landauer <matthew@openaustralia.org>
2010-02-10 21:14:40 +1100

    First stab at getting rails port of planningalerts running on the test instance using passenger

--- cookbooks/planningalerts/attributes/planningalerts.rb
+++ cookbooks/planningalerts/attributes/planningalerts.rb
@@ -11,7 +11,9 @@ planningalerts[:test][:subdomain] = "test" unless planningalerts[:test].has_key?
   planningalerts[stage][:name] = "#{planningalerts[stage][:subdomain]}.planningalerts" unless planningalerts[stage].has_key?(:name)
   planningalerts[stage][:virtual_host_name] = "#{planningalerts[stage][:subdomain]}.#{planningalerts[:domain]}" unless planningalerts[stage].has_key?(:virtual_host_name)
   planningalerts[stage][:install_path] = "/www/#{planningalerts[stage][:name]}/app" unless planningalerts[stage].has_key?(:install_path)
-  planningalerts[stage][:html_root] = "#{planningalerts[stage][:install_path]}/current/planningalerts-app/docs" unless planningalerts[stage].has_key?(:html_root)
 end
+
+planningalerts[:production][:html_root] = "#{planningalerts[:production][:install_path]}/current/planningalerts-app/docs" unless planningalerts[:production].has_key?(:html_root)
+planningalerts[:test][:html_root] = "#{planningalerts[:test][:install_path]}/current/planningalerts-app/public" unless planningalerts[:test].has_key?(:html_root)
 
 planningalerts[:test][:apache_password_file] = "#{planningalerts[:test][:install_path]}/shared/htpasswd" unless planningalerts[:test].has_key?(:apache_password_file)
--- cookbooks/planningalerts/recipes/default.rb
+++ cookbooks/planningalerts/recipes/default.rb
@@ -55,10 +55,19 @@ package "p5-HTML-Element-Extended" do
   source "ports"
 end
 
-remote_file @node[:planningalerts][:test][:apache_password_file] do
-  source "htpasswd"
-  mode 0644
+gem_package "rails" do
+  version "2.3.5"
 end
+
+gem_package "mysql"
+
+# The gems we need for running the rails app are also stored in the app
+# For the time being we'll use chef to install the gems rather than running "rake gems:install"
+gem_package "haml"
+gem_package "geokit"
+gem_package "shorturl"
+gem_package "nokogiri"
+gem_package 'email_spec'
 
 # For the time being only setting up the staging environment (:test)
 [:production, :test].each do |stage|
@@ -68,54 +77,92 @@ end
     mode 0755
     recursive true
   end
+end
 
-  directory "#{node[:planningalerts][stage][:install_path]}/shared/data" do
-    owner "www"
-    group "www"
-    # Making this writeable by everybody so that the mailer can be run as any user
-    # Just a convenience thing. Really should look at this in more detail
-    mode 0777
-    recursive true
-  end
-  
-  template "#{@node[:planningalerts][stage][:install_path]}/shared/config.php" do
-    source "config.php.erb"
-    owner "matthewl"
-    group "matthewl"
-    mode 0644
-    variables :stage => stage
-  end
-  
-  template "#{@node[:planningalerts][stage][:install_path]}/shared/htaccess" do
-    source "htaccess.erb"
-    owner "matthewl"
-    group "matthewl"
-    mode 0644
-    variables :stage => stage
-  end
+directory "#{node[:planningalerts][:production][:install_path]}/shared/data" do
+  owner "www"
+  group "www"
+  # Making this writeable by everybody so that the mailer can be run as any user
+  # Just a convenience thing. Really should look at this in more detail
+  mode 0777
+  recursive true
+end
 
-  directory "#{@node[:planningalerts][stage][:install_path]}/shared/pids"
-  
-  # TODO: Restart Passenger after deploy
+template "#{@node[:planningalerts][:production][:install_path]}/shared/config.php" do
+  source "config.php.erb"
+  owner "matthewl"
+  group "matthewl"
+  mode 0644
+  variables :stage => :production
+end
 
-  # Going to try to use the new deploy resource instead of using capistrano. Let's see how we go
-  deploy_revision node[:planningalerts][stage][:install_path] do
-    revision stage.to_s
-    repo "git://git.openaustralia.org/planningalerts.git"
-    # This should not be set to :force_deploy for normal usage
-    #action :force_deploy
-    scm_provider Chef::Provider::Git
-    # Override the default rails-like structure
-    symlink_before_migrate "config.php" => "planningalerts-app/docs/include/config.php"
-    purge_before_symlink ["planningalerts-app/docs/scrapers"]
-    create_dirs_before_symlink ["planningalerts-parsers/tmp", "planningalerts-parsers/public"]
-    symlinks "htaccess" => "planningalerts-app/docs/.htaccess",
-      "../current/planningalerts-parsers/public" => "planningalerts-app/docs/scrapers",
-      "pids" => "planningalerts-parsers/tmp/pids",
-      "log" => "planningalerts-parsers/log"
-    enable_submodules true
-  end
+template "#{@node[:planningalerts][:production][:install_path]}/shared/htaccess" do
+  source "htaccess.erb"
+  owner "matthewl"
+  group "matthewl"
+  mode 0644
+  variables :stage => :production
+end
 
+directory "#{@node[:planningalerts][:production][:install_path]}/shared/pids"
+
+# TODO: Restart Passenger after deploy
+
+# Going to try to use the new deploy resource instead of using capistrano. Let's see how we go
+deploy_revision node[:planningalerts][:production][:install_path] do
+  revision "production"
+  repo "git://git.openaustralia.org/planningalerts.git"
+  # This should not be set to :force_deploy for normal usage
+  #action :force_deploy
+  scm_provider Chef::Provider::Git
+  # Override the default rails-like structure
+  symlink_before_migrate "config.php" => "planningalerts-app/docs/include/config.php"
+  purge_before_symlink ["planningalerts-app/docs/scrapers"]
+  create_dirs_before_symlink ["planningalerts-parsers/tmp", "planningalerts-parsers/public"]
+  symlinks "htaccess" => "planningalerts-app/docs/.htaccess",
+    "../current/planningalerts-parsers/public" => "planningalerts-app/docs/scrapers",
+    "pids" => "planningalerts-parsers/tmp/pids",
+    "log" => "planningalerts-parsers/log"
+  enable_submodules true
+end
+
+directory "#{node[:planningalerts][:test][:install_path]}/shared/" do
+  owner "matthewl"
+  group "matthewl"
+end
+
+directory "#{node[:planningalerts][:test][:install_path]}/shared/pids" do
+  owner "matthewl"
+  group "matthewl"
+end
+
+directory "#{node[:planningalerts][:test][:install_path]}/shared/log" do
+  mode 0777
+end
+
+directory "#{node[:planningalerts][:test][:install_path]}/shared/config"
+
+template "#{@node[:planningalerts][:test][:install_path]}/shared/config/database.yml" do
+  source "database.yml.erb"
+end
+
+deploy_revision node[:planningalerts][:test][:install_path] do
+  revision "test"
+  user "matthewl"
+  group "matthewl"
+  repo "git://git.openaustralia.org/planningalerts.git"
+  # This should not be set to :force_deploy for normal usage
+  #action :force_deploy
+  scm_provider Chef::Provider::Git
+  symlink_before_migrate "config/database.yml" => "planningalerts-app/config/database.yml"
+  purge_before_symlink ["planningalerts-app/log", "planningalerts-app/tmp/pids", "planningalerts-app/public/system"]
+  create_dirs_before_symlink ["planningalerts-app/tmp", "planningalerts-app/public", "planningalerts-app/config"]
+  symlinks "system" => "planningalerts-app/public/system", "pids" => "planningalerts-app/tmp/pids",
+    "log" => "planningalerts-app/log"
+  enable_submodules true
+end
+
+[:production, :test].each do |stage|
   template "#{@node[:apache][:dir]}/sites-available/#{@node[:planningalerts][stage][:name]}" do
     source "apache_#{stage}.conf.erb"
     mode 0644
@@ -125,6 +172,11 @@ end
   end
 
   apache_site @node[:planningalerts][stage][:name]
+end
+
+remote_file @node[:planningalerts][:test][:apache_password_file] do
+  source "htpasswd"
+  mode 0644
 end
 
 # For deployments code is stored on the server. So, for testing make sure that you push the code
